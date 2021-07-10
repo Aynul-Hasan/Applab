d2afe2c320224015088d204148f18e789ad3ece3
Aynul-Hasan <aynulhasan003gmail.com>
2021-07-10 17:10:30 +0600

    js change

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
-const scrollBtn= document.querySelector(".srcl");
+const scrollBtn= document.getElementById('scrl');
 
 
-scrollBtn.addEventListener("click", ()=>{
-    window.scrollTo(0,0)
-})
+window.onscroll = ()=> {scrollFunction()};
+
+const scrollFunction=()=> {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+const topFunction=()=>{
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
